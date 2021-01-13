1fbd85f4d17137ea068550d23271ff495a1c6010
FroYoMan <34078793+FroYoMan@users.noreply.github.com>
2021-01-13 08:32:49 -0700

    Add project files.

---
+++ Operators1.vb
@@ -0,0 +1,14 @@
+﻿Module Operators1
+
+    Sub Main()
+        Console.WriteLine(2 + 2)
+        Console.WriteLine(9 - 2)
+        Console.WriteLine(5 * 2)
+
+        Console.WriteLine(18 / 6) 'Regular Division
+        Console.WriteLine(18 / 6) 'Integer only Division
+        Console.WriteLine(18 Mod 6) 'Just the remainder
+        Console.ReadLine()
+    End Sub
+
+End Module
