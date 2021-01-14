a872e5c2d14d17c23f9f5e7bb38c88a36fd22eb6
FroYoMan <34078793+FroYoMan@users.noreply.github.com>
2021-01-14 08:43:25 -0700

    Boolean Comparisons

--- Operators1.vb
+++ Operators1.vb
@@ -9,6 +9,17 @@
         Console.WriteLine(18 / 6) 'Regular Division
         Console.WriteLine(18 / 6) 'Integer only Division
         Console.WriteLine(18 Mod 6) 'Just the remainder
+
+        Console.WriteLine("banana" & " apples") '& and + both work as concatenate but & is more right
+        Console.WriteLine("banana" + "5")
+
+        Console.WriteLine(4 > 3)
+        Console.WriteLine(4 < 3)
+        Console.WriteLine(4 = 3)
+        Console.WriteLine(3 <> 3)
+
+        Console.WriteLine("B" > "AAA")
+
         Console.ReadLine()
     End Sub
 
